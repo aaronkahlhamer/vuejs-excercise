0f5db2f7e2246ac7f3dc645645a7d0bbae83b5ea
Aaron K <aaronkahlhamer@gmail.com>
2023-07-27 12:27:27 -0500

    Enhanced CSS structure and styling
    - Refactored code to separate styles into a dedicated file, improving maintainability and organization.
    - Implemented new styling to enhance the visual appeal and user experience.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Vue, { VNode } from "vue";
 import App from "./App.vue";
 import "@jsonforms/vue2-vanilla/vanilla.css";
+import "./assets/css/main.css";
 
 Vue.config.productionTip = false;
 
